5cc392d3c9303c194494efe7b0bd241b5e44b250
Chrism7 <chrism@mi.fu-berlin.de>
2019-02-21 00:51:47 +0100

    minor changes (cleaning)

--- pypuf/learner/neural_networks/mlp.py
+++ pypuf/learner/neural_networks/mlp.py
@@ -1,5 +1,6 @@
 from numpy import reshape, sign
 import os
+from keras.backend import mean, sign as sign_keras
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
@@ -44,14 +45,18 @@ class MultiLayerPerceptron(Learner):
             self.training_set.challenges = reshape(self.training_set.challenges, (self.training_set.N, in_shape))
             self.validation_set.challenges = reshape(self.validation_set.challenges, (self.validation_set.N, in_shape))
         self.clf = Sequential()
-        self.clf.add(Dense(self.n + 2**self.k, activation='relu', input_dim=in_shape))
-        self.clf.add(Dense(self.n + 2**self.k, activation='relu'))
-        self.clf.add(Dense(self.n + 2**self.k, activation='relu'))
+        self.clf.add(Dense(2**self.k, activation='relu', input_dim=in_shape))
+        self.clf.add(Dense(2**self.k, activation='relu'))
+        self.clf.add(Dense(2**self.k, activation='relu'))
         self.clf.add(Dense(1, activation='sigmoid'))
+
+        def pypuf_accuracy(y_true, y_pred):
+            return .5 * mean(sign_keras(y_true * y_pred)) + .5
+
         self.clf.compile(
             optimizer=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, amsgrad=True),
             loss='binary_crossentropy',
-            metrics=['accuracy']
+            metrics=[pypuf_accuracy]
         )
 
         class Model:
@@ -64,14 +69,14 @@ class MultiLayerPerceptron(Learner):
             def eval(self, cs):
                 if self.transformation is not None:
                     cs = reshape(self.transformation(cs, self.k), (len(cs), self.k * self.n))
-                return sign(self.clf.predict(cs)[:, 0])
+                return sign(self.clf.predict(cs)).flatten()
 
         self.model = Model(self.clf, self.n, self.k, self.transformation)
 
     def learn(self):
-        earlystop = EarlyStopping(monitor='loss', min_delta=0.0001, patience=10,
-                                  verbose=1, mode='auto', restore_best_weights=True)
-        callbacks = [earlystop]
+        early_stop = EarlyStopping(monitor='val_pypuf_accuracy', min_delta=0.0001, patience=10,
+                                   verbose=1, mode='auto', restore_best_weights=True)
+        callbacks = [early_stop]
         self.history = self.clf.fit(
             x=self.training_set.challenges,
             y=self.training_set.responses,
--- studies/exec_input_transform_comparison_mlp_keras.py
+++ studies/exec_input_transform_comparison_mlp_keras.py
@@ -11,31 +11,17 @@ SEED_RANGE = 2 ** 32
 SAMPLES_PER_POINT = 10
 BATCH_SIZE = 1000
 ITERATION_LIMIT = 1000
-LOG_NAME = 'keras_mlp_DO'
+LOG_NAME = 'keras_mlp'
 
 
 definitions = [
-    # k  n   CRP_min max
-    [1, 64, (1000, 3000), False, None, None, None, None],
-    #[2, 64, (400, 8000), False, None, None, None, None],
-    #[3, 64, (1600, 32000), False, None, None, None, None],
-    #[4, 64, (6400, 128000), False, None, None, None, None],
+    [4, 64, (400000, 800000), True, 1, 2, 3, 4],
 ]
 
 
-for d in definitions:
+for k, n, (min_CRPs, max_CRPs), preprocess, seed_i, seed_c, seed_m, seed_a in definitions:
 
-    k = d[0]
-    n = d[1]
-    min_CRPs = d[2][0]
-    max_CRPs = d[2][1]
-    preprocess = d[3]
-    seed_i = RandomState(d[4]).randint(SEED_RANGE)
-    seed_c = RandomState(d[5]).randint(SEED_RANGE)
-    seed_m = RandomState(d[6]).randint(SEED_RANGE)
-    seed_a = RandomState(d[7]).randint(SEED_RANGE)
     log_name = LOG_NAME + '_{0}_{1}'.format(k, n)
-
     experiments = list()
     parameters = list()
 
