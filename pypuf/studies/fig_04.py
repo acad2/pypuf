563af995108125c37f2decd6930b7be84d08aa8a
Nils Wisiol <mail@nils-wisiol.de>
2019-03-05 13:56:38 +0100

    Added fig 04 plots using seaborn

--- pypuf/studies/fig_04.py
+++ pypuf/studies/fig_04.py
@@ -1,12 +1,15 @@
 from pypuf.experiments.experiment.logistic_regression import ExperimentLogisticRegression, Parameters
 from pypuf.studies.base import Study
+from matplotlib import pyplot
+from seaborn import distplot
+from numpy.ma import arange
 
 
 class Fig04(Study):
 
     CRPS = 30000
     SAMPLE_SIZE = 100
-    LR_TRANSFORMATIONS = ['id', 'random', 'lightweight_secure', 'fixed_permutation']
+    LR_TRANSFORMATIONS = ['id', 'atf', 'random', 'lightweight_secure', 'fixed_permutation']
     SIZE = (64, 4)
 
     SHUFFLE = True
@@ -42,4 +45,23 @@ class Fig04(Study):
         # TODO add correlation attack
 
     def plot(self):
-        print(self.experimenter.results[['transformation', 'accuracy']])
+        groups = self.experimenter.results.groupby(['transformation'])
+        figure, axes = pyplot.subplots(nrows=len(groups), ncols=1)
+        figure.subplots_adjust(hspace=3)
+        figure.set_size_inches(w=6, h=2 * len(groups))
+        for axis, (transformation, group_results) in zip(axes, groups):
+            axis.set_title(transformation)
+            axis.set_xlim([.48, 1])
+            axis.set_ylabel('rel. frequency')
+            distplot(
+                group_results[['accuracy']],
+                ax=axis,
+                kde=False,
+                bins=arange(.48, 1.0, .02),
+                hist=True,
+                norm_hist=True,
+            )
+        axes[-1].set_xlabel('accuracy')
+        figure.tight_layout()
+        figure.savefig('figures/' + self.name() + '.pdf')
+        pyplot.close(figure)
