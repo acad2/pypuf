27e24146d9c38f040a3f24fa3048b4b57d1892f5
Nils Wisiol <mail@nils-wisiol.de>
2019-03-05 17:07:15 +0100

    improve fig 04

--- pypuf/studies/fig_04.py
+++ pypuf/studies/fig_04.py
@@ -2,17 +2,24 @@ from pypuf.experiments.experiment.logistic_regression import ExperimentLogisticR
 from pypuf.studies.base import Study
 from matplotlib import pyplot
 from seaborn import distplot
-from numpy.ma import arange
+from numpy import arange, ones_like
 
 
 class Fig04(Study):
 
     CRPS = 30000
-    SAMPLE_SIZE = 100
-    LR_TRANSFORMATIONS = ['id', 'atf', 'random', 'lightweight_secure', 'fixed_permutation']
+    SAMPLE_SIZE = 110
+    LR_TRANSFORMATIONS = ['atf', 'random', 'lightweight_secure', 'fixed_permutation']
     SIZE = (64, 4)
 
     SHUFFLE = True
+
+    NICE_TRANSFORMATION_NAMES = {
+        'atf': 'Classic',
+        'fixed_permutation': 'Permutation-Based',
+        'lightweight_secure': 'Lightweight Secure',
+        'random': 'Pseudorandom',
+    }
 
     def name(self):
         return 'fig_04'
@@ -45,23 +52,30 @@ class Fig04(Study):
         # TODO add correlation attack
 
     def plot(self):
-        groups = self.experimenter.results.groupby(['transformation'])
+        results = self.experimenter.results
+        groups = results[results['transformation'] != 'id'].groupby(['transformation'])
         figure, axes = pyplot.subplots(nrows=len(groups), ncols=1)
         figure.subplots_adjust(hspace=3)
-        figure.set_size_inches(w=6, h=2 * len(groups))
+        figure.set_size_inches(w=5, h=1.5 * len(groups))
         for axis, (transformation, group_results) in zip(axes, groups):
-            axis.set_title(transformation)
+            axis.set_title('{} using {:,} CRPs'.format(self.NICE_TRANSFORMATION_NAMES[transformation], self.CRPS))
             axis.set_xlim([.48, 1])
-            axis.set_ylabel('rel. frequency')
             distplot(
                 group_results[['accuracy']],
                 ax=axis,
                 kde=False,
-                bins=arange(.48, 1.0, .02),
+                bins=arange(.48, 1.01, .01),
                 hist=True,
-                norm_hist=True,
+                norm_hist=False,
+                color='blue',
+                hist_kws={
+                    'alpha': 1,
+                    # the following line turns the histogram of absolute frequencies into one with relative frequencies
+                    'weights': ones_like(group_results[['accuracy']]) / float(len(group_results[['accuracy']]))
+                }
             )
         axes[-1].set_xlabel('accuracy')
+        axes[(len(groups) - 1) // 2].set_ylabel('rel. frequency')
         figure.tight_layout()
         figure.savefig('figures/' + self.name() + '.pdf')
         pyplot.close(figure)
