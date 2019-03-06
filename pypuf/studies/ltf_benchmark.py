c02ef23ce295f4746d1f52ecda64baad6e59ed04
Nils Wisiol <mail@nils-wisiol.de>
2019-03-06 11:29:20 +0100

    Benchmark for LTFArray evaluation

---
+++ pypuf/studies/ltf_benchmark.py
@@ -0,0 +1,105 @@
+from platform import processor, uname
+
+from matplotlib.pyplot import subplots
+from numpy.distutils import cpuinfo
+
+from pypuf.experiments.experiment.base import Experiment
+from os import getpid
+from typing import NamedTuple, Union
+from uuid import UUID
+from seaborn import relplot
+from pypuf.simulation.arbiter_based.ltfarray import LTFArray
+from pypuf.studies.base import Study
+from pypuf.tools import sample_inputs
+from numpy.random.mtrand import RandomState
+
+
+class Parameters(NamedTuple):
+    n: int
+    k: int
+    N: int
+    transform: str
+    combiner: str
+    seed_input: int
+    hostname: Union[str, None]
+
+
+class Result(NamedTuple):
+    experiment_id: UUID
+    pid: int
+    measured_time: float
+    cpu: Union[str, None]
+
+
+class Benchmark(Experiment):
+
+    SHUFFLE=True
+
+    def __init__(self, progress_log_prefix, parameters):
+        super().__init__(progress_log_prefix, parameters)
+        self.set = None
+        self.ltf_array = None
+
+    def prepare(self):
+        self.set = sample_inputs(self.parameters.n, self.parameters.N, RandomState(seed=self.parameters.seed_input))
+        self.ltf_array = LTFArray(
+            weight_array=LTFArray.normal_weights(self.parameters.n, self.parameters.k),
+            transform=self.parameters.transform,
+            combiner=self.parameters.combiner,
+        )
+
+    def run(self):
+        self.ltf_array.eval(self.set)
+
+    def analyze(self):
+        cpu = None
+        try:
+            cpu = cpuinfo.cpu.info[0]['model name']
+        except:
+            pass
+
+        return Result(
+            experiment_id=self.id,
+            pid=getpid(),
+            measured_time=self.measured_time,
+            cpu=cpu,
+        )
+
+
+class LTFBenchmark(Study):
+
+    Ns = [10000, 20000, 50000, 100000, 200000, 500000, 1000000]
+    TRANSFORMS = ['id', 'atf']
+    SIZE = (64, 4)
+    SAMPLE_SIZE = 100
+
+    def name(self):
+        return 'ltf_benchmark'
+
+    def experiments(self):
+        (n, k) = self.SIZE
+        return [
+            Benchmark(
+                progress_log_prefix=None,
+                parameters=Parameters(
+                    n=n,
+                    k=k,
+                    N=N,
+                    transform=transform,
+                    combiner='xor',
+                    seed_input=314159 + i,
+                    hostname=uname().node,
+                )
+            )
+            for N in self.Ns
+            for transform in self.TRANSFORMS
+            for i in range(self.SAMPLE_SIZE)
+        ]
+
+    def plot(self):
+        results = self.experimenter.results[['cpu', 'hostname', 'transform', 'N', 'measured_time']]
+        groups = results.groupby(['transform'])
+
+        for transform, group in groups:
+            figure = relplot(x='N', y='measured_time', hue='cpu', style='hostname', kind='line', data=group)
+            figure.savefig('figures/%s-%s.pdf' % (self.name(), transform))
